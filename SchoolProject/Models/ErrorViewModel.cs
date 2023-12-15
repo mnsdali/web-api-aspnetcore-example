c68908c0e85cd6228a51713cd100fbbff2407607
mnsdali <mohamedali.mnassar@gmail.com>
2023-12-15 21:07:30 +0100

    Delete SchoolProject directory

--- SchoolProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SchoolProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- SchoolProject/Models/ViewModels/UserRoleViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolProject.Models.ViewModels
-{
-    public class UserRoleViewModel
-    {
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public bool IsSelected { get; set; }
-    }
-}
