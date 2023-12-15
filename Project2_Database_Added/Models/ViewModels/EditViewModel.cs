fad05634b70ecff69538b97c0124c822c8703465
mnsdali <mohamedali.mnassar@gmail.com>
2023-12-15 21:07:22 +0100

    Delete Project2_Database_Added directory

--- Project2_Database_Added/Models/ViewModels/EditViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Project2_Database_Added.Models.ViewModels
-{
-    public class EditViewModel : CreateViewModel
-    {
-        public int Id { get; set; }
-        public string ExistingImagePath { get; set; }
-    }
-}
